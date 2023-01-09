58344064f63c9a6dd70b8746329c3221de7213a7
kiahuaw <122152814+kiahuaw@users.noreply.github.com>
2023-01-09 12:48:46 +0800

    Add files via upload

---
+++ MineSweeper/MineSweepImproved.cpp
@@ -0,0 +1,219 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+#include <graphics.h>
+#include <easyx.h>
+#include <stdlib.h>
+#include "data.h"
+#include "show.h"
+
+int main(void) {
+	InitShowUI();
+	GamePlay();
+	closegraph();
+	return 0;
+}
+void GamePlay() {
+	InitGame();
+	cleardevice();
+	start = time(NULL);
+	while (1) {
+		end = time(NULL);
+		ShowUI();
+		Control();
+		GameWinJudge();
+		if (boom) {
+			ShowUI();
+			break;//如果炸了就退出循环
+		}
+		if (win)break;//如果赢了也退出循环
+	}
+	while (1) {
+		ShowUI();
+		Control();
+	}
+	return;
+}
+void InitGame() {
+	Choose();
+	boom = false;
+	win = false;
+	//记录每个格子左上角的坐标
+	for (int i = 0; i < hang; i++) {
+		for (int j = 0; j < lie; j++) {
+			pos[i][j].x = (j) * 30 + j * 4 + 7;
+			pos[i][j].y = (i + 1) * 30 + i * 4 + 60;
+		}
+	}
+	//随机数播种
+	srand((size_t)time(NULL));
+	//初始化地图元素
+	for (int i = 0; i < hang; i++) 
+		for (int j = 0; j < lie; j++) 
+			Map[i][j] = 0;	
+	for (int i = 0; i < hang; i++) 
+		for (int j = 0; j < lie; j++) 
+			FlagMap[i][j] = 0;			
+	if (choice == 0)initMineNum = 10;
+	if (choice == 1)initMineNum = 40;
+	for (int i = 0; i < initMineNum; i++) {
+		//随机产生10个雷
+		int x = rand() % lie;
+		int y = rand() % hang;
+		while (1) {//如果随机出来的雷与原来的雷位置相同就一直随机，否则退出循环
+			//去重环节
+			if (Map[y][x] == 1) {
+				x = rand() % hang;
+				y = rand() % lie;
+			}
+			else break;
+		}
+		Map[y][x] = 1;
+	}
+	//重置雷的数量
+	MineNum = initMineNum;
+	return;
+}
+void Choose() {
+	if (choice == 0) { hang = 9; lie = 9; }
+	if (choice == 1) { hang = 16; lie = 16; }
+	return;
+}
+void Control() {
+	key = GetMouseMsg();
+	while (1) {
+		KEY_X = key.x;
+		KEY_Y = key.y;
+		if (key.x >= 0 && key.x <= 556 && key.y >= 0 && key.y <= 680) {
+			if (key.uMsg == WM_LBUTTONDOWN) {//左键
+				if (key.x >= pos[0][0].x && key.x <= pos[hang - 1][lie - 1].x + 30 
+				 && key.y >= pos[0][0].y && key.y <= pos[hang - 1][lie - 1].y + 30
+				 && boom == false)
+					Remove(key.x, key.y);//消除函数,深度递归,遇到雷返回
+				if (choice == 0) {
+					if (key.x >= 132 && key.x <= 182 && key.y >= 20 && key.y <= 70)
+						GamePlay();	//重新开始游戏
+				}
+				else if (choice == 1) {//130, 20, 425, 70, 6, 6
+					if (key.x >= 130 && key.x <= 425 && key.y >= 20 && key.y <= 70)
+						GamePlay();	//重新开始游戏
+				}
+				if (choice == 1) {
+					if (key.x >= 15 && key.x <= 200 && key.y >= 643 && key.y <= 672) {
+						choice = 0;
+						main();//重新进入主函数加载窗口大小
+					}
+					if (key.x >= 286 && key.x <= 541 && key.y >= 643 && key.y <= 672) {
+						exit(0);//直接退出程序，下面同理
+					}
+				}
+				if (choice == 0) {
+					if (key.x >= 15 && key.x <= 150 && key.y >= 405 && key.y <= 435) {
+						choice = 1;
+						main();
+					}
+					if (key.x >= 167 && key.x <= 303 && key.y >= 405 && key.y <= 435) {
+						exit(0);
+					}
+				}				
+			}
+			if (key.uMsg == WM_RBUTTONDOWN) {//右键
+				if (key.x >= pos[0][0].x && key.x <= pos[hang - 1][lie - 1].x + 30 
+				 && key.y >= pos[0][0].y && key.y <= pos[hang - 1][lie - 1].y + 30
+				 && boom == false) {
+					FlagAndQmark(key.x, key.y);
+				}
+			}
+		}
+		else break;
+		break;//多break有好处
+	}
+	return;
+}
+void FlagAndQmark(int x, int y) {
+	for (int i = 0; i < hang; i++) {
+		for (int j = 0; j < lie; j++) {//判断点击的在第几行第几列   i行   j列 
+			if (x >= pos[i][j].x && x <= pos[i][j].x + 30 && y >= pos[i][j].y && y <= pos[i][j].y + 30) {
+				if (FlagMap[i][j] == 0 && FlagNumber()) {
+					FlagMap[i][j] += 10;
+					MineNum--;
+				}
+				else if (FlagMap[i][j] == 10 || FlagMap[i][j] == 11) {
+					FlagMap[i][j]++;
+					if (FlagMap[i][j] == 11)MineNum++;
+				}
+				if (FlagMap[i][j] == 12) FlagMap[i][j] = 0;
+			}
+		}
+	}
+	return;
+}
+int MineNumber(int y, int x) {//简单的防止数组越界的操作返回雷的个数
+	if (y == 0 && x == 0)return Map[y][x + 1] + Map[y + 1][x] + Map[y + 1][x + 1];
+	if (y == 0) return Map[y][x + 1] + Map[y][x - 1] + Map[y + 1][x - 1] + Map[y + 1][x] + Map[y + 1][x + 1];
+	else if (x == 0)return Map[y + 1][x] + Map[y - 1][x] + Map[y - 1][x + 1] + Map[y + 1][x + 1] + Map[y][x + 1];
+	else return Map[y][x + 1] + Map[y][x - 1] + Map[y + 1][x] + Map[y - 1][x]
+		+ Map[y + 1][x + 1] + Map[y + 1][x - 1] + Map[y - 1][x + 1] + Map[y - 1][x - 1];
+}
+void Expand(int y, int x) {
+	FlagMap[y][x] = 9;//9是打开的状态	
+	int minecount = MineNumber(y, x);
+	if (minecount == 0) {
+		for (int i = y - 1; i <= y + 1; i++) {
+			for (int j = x - 1; j <= x + 1; j++) {//在游戏区域内且附近的块可以打开
+				if (i >= 0 && i < hang && j >= 0 && j < lie && FlagMap[i][j] == 0) {
+					FlagMap[i][j] = 9;//打开
+					Expand(i, j);//对可以打开的块的位置继续往下展开
+				}
+			}
+		}
+	}
+	else //展开到有雷的地方就输入周围8个格子内有几个雷
+		FlagMap[y][x] = minecount;
+}
+void Remove(int x, int y) {
+	for (int i = 0; i < hang; i++) {
+		for (int j = 0; j < lie; j++) {//判断点击的在第几行第几列   i行   j列 
+			if (x >= pos[i][j].x && x <= pos[i][j].x + 30 && y >= pos[i][j].y && y <= pos[i][j].y + 30) {
+				if (Map[i][j] == 1) {
+					boom = true;
+					return;//点到雷直接结束游戏
+				}
+				if (FlagMap[i][j] != 0) return;//点击空白处不能打开方块
+				Expand(i, j);//进入自动展开函数
+			}
+		}
+	}
+	return;
+}
+int MineWatch() {
+	int count = 0;//返回周围8格子内qizi的个数
+	for (int i = 0; i < hang; i++) {
+		for (int j = 0; j < lie; j++) {
+			if (FlagMap[i][j] == 10) count++;
+		}
+	}
+	return count;
+}
+int GameWinJudge() {//胜利判断
+	int all = 0;
+	int Boom = 0;
+	for (int i = 0; i < hang; i++) {
+		for (int j = 0; j < lie; j++) {
+			if (Map[i][j] == 1 && FlagMap[i][j] == 10) Boom++;
+			for (int k = 1; k <= 9; k++) {
+				if (FlagMap[i][j] == k)all++;
+			}
+		}
+	}
+	if (Boom == 10 || all == (width * height - initMineNum)) win = true;//有十个旗子或者除雷之外的格子都被点开就获胜
+	return 0;
+}
+int FlagNumber() {//监视全局旗子的个数防止竖旗超过10个
+	int count = 0;
+	for (int i = 0; i < hang; i++)
+		for (int j = 0; j < lie; j++)
+			if (FlagMap[i][j] == 10) count++;
+	if (count == initMineNum)return 0;
+	else return 1;
+}
---
+++ MineSweeper/show.h
@@ -0,0 +1,197 @@
+#pragma once
+#include <graphics.h>
+#include "data.h"
+
+void InitShowUI() {
+	if (choice == 0) { width = 318, height = 440; }
+	if (choice == 1) { width = 556, height = 680; }
+	initgraph(width, height);//创建窗口
+	setbkcolor(RGB(161, 97, 26));
+	cleardevice();
+	return;
+}
+void ShowUI() {
+	BeginBatchDraw();//防止绘图闪烁
+	cleardevice();
+	if (choice == 0) {
+		setbkmode(TRANSPARENT);
+		//难度
+		setfillcolor(RGB(204, 145, 65));
+		solidrectangle(0, 400, 318, 440);
+		//easy
+		setfillcolor(RGB(251, 217, 130));
+		solidroundrect(15, 405, 150, 435, 6, 6);
+		settextcolor(RGB(196, 133, 54));
+		settextstyle(35, 0, "华文琥珀");
+		outtextxy(25, 405, "MIDDLE");
+		// exit
+		solidroundrect(167, 405, 303, 435, 6, 6);
+		outtextxy(205, 405, "EXIT");
+		//填充功能区
+		setfillcolor(RGB(204, 145, 65));
+		solidrectangle(0, 0, 318, 82);
+		//重新开始图标绘制
+		setfillcolor(RGB(251, 217, 130));
+		solidroundrect(132, 20, 182, 70, 6, 6);
+		//newgame
+		settextcolor(RGB(196, 133, 54));
+		settextstyle(20, 0, "华文琥珀");
+		outtextxy(138, 25, "New");
+		settextstyle(20, 0, "华文琥珀");
+		outtextxy(134, 45, "game");
+		//右上角的雷数
+		settextcolor(RGB(245, 87, 98));
+		char minearr[5] = " ";
+		if (win)sprintf(minearr, "%d", 0);
+		else sprintf(minearr, "%d", MineNum);
+		settextstyle(50, 0, "华文琥珀");
+		setfillcolor(RGB(161, 97, 26));
+		solidroundrect(212, 20, 303, 70, 6, 6);
+		int temp = textwidth(minearr);
+		outtextxy(212 + (303 - 212) / 2 - temp / 2, 23, minearr);
+		//显示时间s
+		char time[5] = " ";
+		sprintf(time, "%d", end - start);
+		int timetemp = textwidth(time);
+		solidroundrect(15, 20, 102, 70, 6, 6);
+		outtextxy(15 + (102 - 15) / 2 - timetemp / 2, 23, time);
+	}
+	if (choice == 1) {
+		setbkmode(TRANSPARENT);
+		//选择难度
+		setfillcolor(RGB(204, 145, 65));
+		solidrectangle(0, 637, 556, 680);
+		//easy
+		setfillcolor(RGB(251, 217, 130));
+		solidroundrect(15, 643, 270, 672, 6, 6);
+		settextcolor(RGB(196, 133, 54));
+		settextstyle(35, 0, "华文琥珀");
+		outtextxy(108, 643, "EASY");
+		//exit
+		solidroundrect(286, 643, 541, 672, 6, 6);
+		outtextxy(383, 643, "EXIT");
+		//填充功能区
+		setfillcolor(RGB(204, 145, 65));
+		solidrectangle(0, 0, 556, 82);
+		//重新开始图标绘制
+		setfillcolor(RGB(251, 217, 130));
+		solidroundrect(130, 20, 425, 70, 6, 6);
+		//newgame
+		settextcolor(RGB(196, 133, 54));	
+		settextstyle(20, 0, "华文琥珀");
+		outtextxy(258, 25, "New");
+		settextstyle(20, 0, "华文琥珀");
+		outtextxy(254, 45, "game");
+		//右上角的雷数
+		settextcolor(RGB(245, 87, 98));
+		char minearr[5] = " ";
+		if (win)sprintf(minearr, "%d", 0);
+		else sprintf(minearr, "%d", MineNum);
+		settextstyle(50, 0, "华文琥珀");
+		setfillcolor(RGB(161, 97, 26));
+		solidroundrect(212 + 240, 20, 303 + 240, 70, 6, 6);
+		int temp = textwidth(minearr);
+		outtextxy(212 + 240 + (303 - 212) / 2 - temp / 2, 23, minearr);
+		//显示时间s
+		char time[5] = " ";
+		sprintf(time, "%d", end - start);
+		int timetemp = textwidth(time);
+		solidroundrect(15, 20, 102, 70, 6, 6);
+		outtextxy(15 + (102 - 15) / 2 - timetemp / 2, 23, time);
+	}
+	//开始绘制图像
+	for (int i = 0; i < hang; i++) {
+		for (int j = 0; j < lie; j++) {//为10是X   11是 ?'
+			if (FlagMap[i][j] == 0 || FlagMap[i][j] == 10 || FlagMap[i][j] == 11) {//等于0是被遮挡状态				
+				setfillcolor(RGB(251, 217, 130));
+				solidroundrect(pos[i][j].x, pos[i][j].y, pos[i][j].x + 30, pos[i][j].y + 30, 6, 6);
+			}
+			if (FlagMap[i][j] > 0 && FlagMap[i][j] <= 9) {//不等于0就用其他覆盖
+				setfillcolor(RGB(196, 133, 54));
+				solidroundrect(pos[i][j].x, pos[i][j].y, pos[i][j].x + 30, pos[i][j].y + 30, 6, 6);
+			}			
+			if (FlagMap[i][j] > 0 && FlagMap[i][j] < 9) {
+				for (int k = 0; k < 8; k++) {
+					if (FlagMap[i][j] == k + 1)//设置颜色
+						settextcolor(ColorArr[k + 1]);
+				}
+				char Mines[5] = " ";
+				sprintf(Mines, "%d", FlagMap[i][j]);
+				settextstyle(30, 0, "华文琥珀");//输出雷的个数
+				outtextxy(pos[i][j].x + 6, pos[i][j].y + 2, Mines);
+			}
+			if (FlagMap[i][j] == 10) {
+				settextstyle(35, 0, "华文琥珀");
+				settextcolor(RGB(228, 75, 86));//旗子画不出来
+				outtextxy(pos[i][j].x - 2, pos[i][j].y - 1, "×");
+			}
+			if (FlagMap[i][j] == 11) {
+				settextstyle(35, 0, "华文琥珀");
+				settextcolor(RGB(228, 75, 86));//问号
+				outtextxy(pos[i][j].x + 6, pos[i][j].y, "?");
+			}
+			if (boom == true && Map[i][j] == 1) {
+				setfillcolor(RGB(245, 87, 98));//游戏结束显示雷
+				solidroundrect(pos[i][j].x, pos[i][j].y, pos[i][j].x + 30, pos[i][j].y + 30, 6, 6);
+			}
+			if (win) {//胜利后雷变绿				
+				if (Map[i][j] == 1) {
+					if (p > 138)p -= 1;
+					if (q > 226)q += 1;
+					if (u > 138)u += 1;
+					setfillcolor(RGB(p, q, u));
+					solidroundrect(pos[i][j].x, pos[i][j].y, pos[i][j].x + 30, pos[i][j].y + 30, 6, 6);
+					if (p > 138 && q > 226 && u > 138)Sleep(1);
+				}
+			}
+		}
+	}
+	MouseVision();
+	EndBatchDraw();
+	return;
+}
+void MouseVision() {//显示鼠标位置：其实就是给鼠标所在的格子加个框框
+	BeginBatchDraw();//防止绘图闪烁
+	setlinecolor(RGB(161, 97, 26));
+	for (int i = 0; i < hang; i++) {
+		for (int j = 0; j < lie; j++) {
+			if (KEY_X >= pos[i][j].x && KEY_X <= pos[i][j].x + 30 && KEY_Y >= pos[i][j].y && KEY_Y <= pos[i][j].y + 30) {
+				roundrect(pos[i][j].x, pos[i][j].y, pos[i][j].x + 30, pos[i][j].y + 30, 6, 6);
+				roundrect(pos[i][j].x + 1, pos[i][j].y + 1, pos[i][j].x + 29, pos[i][j].y + 29, 6, 6);
+			}
+		}
+	}
+	if (choice == 0) {//重开按键
+		if (KEY_X >= 132 && KEY_X <= 182 && KEY_Y >= 20 && KEY_Y <= 70) {
+			roundrect(132, 20, 182, 70, 6, 6);
+			roundrect(133, 21, 181, 69, 6, 6);
+		}
+	}
+	else if (choice == 1) {//130, 20, 425, 70, 6, 6
+		if (KEY_X >= 130 && KEY_X <= 425 && KEY_Y >= 20 && KEY_Y <= 70) {
+			roundrect(130, 20, 425, 70, 6, 6);
+			roundrect(131, 21, 424, 69, 6, 6);
+		}
+	}
+	if (choice == 1) {//11111
+		if (KEY_X >= 15 && KEY_X <= 270 && KEY_Y >= 643 && KEY_Y <= 672) {
+			roundrect(15, 643, 270, 672, 6, 6);
+			roundrect(16, 644, 269, 671, 6, 6);
+		}
+		if (KEY_X >= 286 && KEY_X <= 541 && KEY_Y >= 643 && KEY_Y <= 672) {
+			roundrect(286, 643, 541, 672, 6, 6);
+			roundrect(287, 644, 540, 671, 6, 6);
+		}
+	}
+	 else if (choice == 0) {//00000
+		if (KEY_X >= 15 && KEY_X <= 150 && KEY_Y >= 405 && KEY_Y <= 435) {
+			roundrect(15, 405, 150, 435, 6, 6);
+			roundrect(16, 406, 149, 434, 6, 6);
+		}
+		if (KEY_X >= 167 && KEY_X <= 303 && KEY_Y >= 405 && KEY_Y <= 435) {
+			roundrect(167, 405, 303, 435, 6, 6);
+			roundrect(168, 406, 302, 434, 6, 6);
+		}
+	}
+	EndBatchDraw();
+}
